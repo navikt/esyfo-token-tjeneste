a28f7851f4234e9c11ba3a7dce6d2d59a1073879
André Wittwer <andre.wittwer@nav.no>
2026-09-16 10:29:57 +0200

    fix: build

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val maskinportenClientVersion = "0.3.0-SNAPSHOT"
 plugins {
     kotlin("jvm") version "2.4.10"
     kotlin("plugin.serialization") version "2.4.10"
-    id("io.ktor.plugin") version ktorVersion
+    id("io.ktor.plugin") version "3.5.2"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
 
